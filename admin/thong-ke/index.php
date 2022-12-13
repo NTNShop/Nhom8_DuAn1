00b9d83a4c207a19e588ae354c10079133aeb42f
nguyenhuuphat1512 <107569407+nguyenhuuphat1512@users.noreply.github.com>
2022-12-13 19:58:48 +0700

    cssadmin

--- admin/thong-ke/index.php
+++ admin/thong-ke/index.php
@@ -24,29 +24,24 @@ require_once "../dao/thong-ke.php";
 
             <div class="boxes">
                 <div class="box box1">
-                    <i class="uil uil-thumbs-up"></i>
                     <span class="text">Product</span>
                     <span class="number">
                         <?php echo thong_ke_hang_hoa() ?>
                     </span>
                 </div>
                 <div class="box box2">
-                    <i class="uil uil-comments"></i>
                     <span class="text">Cate</span>
                     <span class="number"><?php echo thong_ke_loai() ?></span>
                 </div>
                 <div class="box box3">
-                    <i class="uil uil-share"></i>
                     <span class="text">Comments</span>
                     <span class="number"><?php echo thong_ke_binh_luan() ?></span>
                 </div>
                 <div class="box box4">
-                    <i class="uil uil-user"></i>
                     <span class="text">Customer</span>
                     <span class="number"><?php echo thong_ke_khach_hang() ?></span>
                 </div>
                 <div class="box box5">
-                    <i class="uil uil-user"></i>
                     <span class="text">Receipt</span>
                     <span class="number"><?php echo thong_ke_don_hang() ?></span>
                 </div>
