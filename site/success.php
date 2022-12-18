894d7bf6764c7efa95cb760feb089d4c2d64da3a
Ngô Nhân <108146054+NTNShop@users.noreply.github.com>
2022-12-18 21:49:29 +0700

    css

--- site/success.php
+++ site/success.php
@@ -5,10 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        h3{
+            font-weight: bolder;
+            font-size: 30px;
+            text-align: center;
+            margin-top: 10px;
+        }
+        .ql a{
+            
+            text-decoration: none;
+            color: black;
+        }
+        .ql a:hover{
+            color: orangered;
+            font-size: 25px;
+        }
+    </style>
 </head>
 <body>
     <h3>ĐẶT HÀNG THÀNH CÔNG</h3>
-    <a href="index.php">Quay lại trang chủ</a>
+    <div class="ql">
+    <a href="index.php">Quay lại trang chủ</a> - 
     <a href="?lich-su">Xem lại lịch sử đặt hàng</a>
+    </div>
+    
 </body>
 </html>
